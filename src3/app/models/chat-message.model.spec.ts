0686575abd1037a0e147c0ef15c3cd49da984c9f
Macintosh98 <zambareabhishek@gmail.com>
2019-10-15 21:56:15 +0530

    re init

--- src3/app/models/chat-message.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ChatMessage } from './chat-message.model';
-
-describe('ChatMessage', () => {
-  it('should create an instance', () => {
-    expect(new ChatMessage()).toBeTruthy();
-  });
-});
--- src3/app/models/chat-message.model.ts
+++
@@ -1,7 +0,0 @@
-export class ChatMessage {
-    $key?: string;
-    email?: string;
-    userName?: string;
-    message?: string;
-    timeSent?: Date = new Date();
-}
